5e68b5c477fc3ce1aeb0f7926168553c1cfde635
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-05-20 22:18:03 +0300

    feat:(added cors)

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
 const countryRouter = require("./routers/countryRoute");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(logger("dev"));
+app.use(cors());
 
 app.use("/country", countryRouter);
 
