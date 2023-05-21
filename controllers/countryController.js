89f0dc7698230c70d31f2f5e487b3d2aaabb973a
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-05-21 09:20:42 +0300

    feat:(added more controllers)

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -14,13 +14,82 @@ exports.createCountry = async (req, res) => {
     });
     console.log(data);
   } catch (err) {
-    console.log("an error occured", err);
+    res.status(400).json({
+      status: "failure",
+      data: {
+        message: `An error occurred: ${err}`,
+      },
+    });
+    console.log("an error occurred", err);
   }
 };
 
-exports.getCountry = async (req, res) => {
+exports.getCoutries = async (req, res) => {
   try {
+    const countries = await Country.find();
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: countries,
+      },
+    });
   } catch (err) {
+    res.status(400).json({
+      status: "failure",
+      data: {
+        message: `An error occurred: ${err}`,
+      },
+    });
+    console.log("an error occurred", err);
+  }
+};
+
+exports.updateCountry = async (req, res) => {
+  try {
+    const updatedData = await Country.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    console.log(req.body);
+    console.log(updatedData);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: updatedData,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failure",
+      data: {
+        message: `An error occurred: ${err}`,
+      },
+    });
+    console.log("an error occurred", err);
+  }
+};
+
+exports.deleteCountry = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    await Country.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      message: "country deleted",
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failure",
+      data: {
+        message: `An error occurred: ${err}`,
+      },
+    });
     console.log("an error occurred", err);
   }
 };
