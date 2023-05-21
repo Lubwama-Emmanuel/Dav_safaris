3e3f19ff431cd0a9e9a8a3c8659f0767bae81198
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-05-21 09:21:03 +0300

    feat:(added more routers)

--- routers/countryRoute.js
+++ routers/countryRoute.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const countryController = require("../controllers/countryController");
 
 router.post("/createCountry", countryController.createCountry);
+router.get("/getAllCountries", countryController.getCoutries);
+router.patch("/updateCountry/:id", countryController.updateCountry);
+router.delete("/deleteCountry/:id", countryController.deleteCountry);
 
 module.exports = router;
